753be1112fccde6458d78be4a0db12d290f2f0eb
Óscar Arroyo Aguadero <oscararroyoaguadero.guadalupe@alumnado.fundacionloyola.net>
2023-12-14 17:52:39 +0100

    Cambios en vistas

--- vistas/juego.php
+++ vistas/juego.php
@@ -4,9 +4,6 @@ session_start();
 // Verificar si el usuario ha iniciado sesión y tiene el perfil de juego
 if (isset($_SESSION['idusuario']) && isset($_SESSION['perfil']) && $_SESSION['perfil'] == 'JUEGO') {
     // El usuario tiene acceso a la página de juego
-
-    // Puedes agregar aquí el contenido de la página de juego
-
     echo "<h2>Bienvenido al Juego, {$_SESSION['nombre']}!</h2>";
     echo "<p>¡Disfruta de tu experiencia de juego!</p>";
     echo "<p><a href='index.php?accion=logout'>Cerrar sesión</a></p>";
--- vistas/panel_administracion.php
+++ vistas/panel_administracion.php
@@ -3,33 +3,13 @@ session_start();
 
 // Verificar si el usuario ha iniciado sesión y tiene el perfil de administrador
 if (isset($_SESSION['idusuario']) && isset($_SESSION['perfil']) && $_SESSION['perfil'] == 'ADMIN') {
-
     echo "<h2>Bienvenido al Panel de Administración, {$_SESSION['nombre']}!</h2>";
     echo "<p>Aquí puedes realizar tareas administrativas.</p>";
     echo "<p><a href='index.php?controlador=controlador_registro&accion=registro'>Registrar administradores</a>.</p>    ";
     echo "<p><a href='index.php?accion=logout'>Cerrar sesión</a></p>";
 } else {
-    echo "<h1>¿Qué haces aquí?</h1></br>";
-    echo "<img src='https://editorial01.shutterstock.com/preview-440/437041a/a1999462/Shutterstock_437041a.jpg'>";
-    echo "<audio autoplay loop>
-        <source src='vistas/heehee.mp3' type='audio/mp3'>
-        Tu navegador no soporta audio HTML5.
-    </audio>";
-
-    // Código JavaScript para cambiar el color del fondo cada 0.1 segundos
-    echo "<script>
-            setInterval(function(){
-                document.body.style.backgroundColor = getRandomColor();
-            }, 1);
-
-            function getRandomColor() {
-                var letters = '0123456789ABCDEF';
-                var color = '#';
-                for (var i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
-                }
-                return color;
-            }
-          </script>";
+    // Si el usuario no tiene permisos de administrador, redirigirlo a otra página
+    header('location: index.php?accion=logout');
+    exit();
 }
 ?>
